c8ea963b2c66fbb1b719ee946832260677e49c39
Paul J. Morris <mole@morris.net>
2017-01-04 16:22:25 -0500

    ISSUE: Redine 283 PURPOSE: Incrementing version 1.2.1-SNAPSHOT for ongoing work. DESCRIPTION: in pom and constant.

--- docs_manual/sql/mysql_1.1.0_to_1.2.0_changes.sql
+++ docs_manual/sql/mysql_1.1.0_to_1.2.0_changes.sql
@@ -1,6 +1,7 @@
 -- New fields for Specimen table to support verbatim data capture and external verbatim capture loops.
--- Proposed changes, not added yet as of version 1.1.0
+-- Changes not present in version 1.1.0
 -- Added in 1.2.0-SNAPSHOT
+-- Changes present in version 1.2.0  
 alter table Specimen add column verbatimCollector varchar(2000) not null default "";
 alter table Specimen add column verbatimCollection varchar(2000) not null default "";
 alter table Specimen add column verbatimNumbers varchar(2000) not null default "";
