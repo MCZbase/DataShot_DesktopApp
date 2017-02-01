07172ad4c89d357a0c3d3e3513f3f4e116144db2
Paul J. Morris <mole@morris.net>
2017-02-01 14:16:48 -0500

    ISSUE: PURPOSE: Can't save new Specimens in MySQL. DESCRIPTION: Removing not null constraint from the added verbatim fields, as it has been from the hibernate configuration.

--- docs_manual/sql/mysql_1.1.0_to_1.2.0_changes.sql
+++ docs_manual/sql/mysql_1.1.0_to_1.2.0_changes.sql
@@ -2,7 +2,7 @@
 -- Changes not present in version 1.1.0
 -- Added in 1.2.0-SNAPSHOT
 -- Changes present in version 1.2.0  
-alter table Specimen add column verbatimCollector varchar(2000) not null default "";
-alter table Specimen add column verbatimCollection varchar(2000) not null default "";
-alter table Specimen add column verbatimNumbers varchar(2000) not null default "";
-alter table Specimen add column verbatimUnclassifiedText text not null default "";
+alter table Specimen add column verbatimCollector varchar(2000) default "";
+alter table Specimen add column verbatimCollection varchar(2000) default "";
+alter table Specimen add column verbatimNumbers varchar(2000) default "";
+alter table Specimen add column verbatimUnclassifiedText text default "";
