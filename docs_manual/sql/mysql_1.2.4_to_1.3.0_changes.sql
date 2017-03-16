c4c71b2a5079d7f56447b5d0ca2e38f86bdcde6a
Paul J. Morris <mole@morris.net>
2017-03-16 14:22:46 -0400

    ISSUE: resolves #13 PURPOSE: Fix template retaining reference to deleted images. DESCRIPTION: DDL to add a foreign key constraint that cascades null to Template.referenceimage.

--- docs_manual/sql/mysql_1.2.4_to_1.3.0_changes.sql
+++ docs_manual/sql/mysql_1.2.4_to_1.3.0_changes.sql
@@ -1,3 +1,7 @@
 alter table Specimen add column verbatimClusterIdentifier varchar(255);
 alter table Specimen add column externalWorkflowProcess varchar(900);
 alter table Specimen add column externalWorkflowDate datetime default null;
+
+ALTER TABLE Template ENGINE=InnoDB;
+update Template set referenceimage = null;
+alter table Template add CONSTRAINT fk_template_refimg FOREIGN KEY (referenceimage) references Image (imageid) ON DELETE SET null;
