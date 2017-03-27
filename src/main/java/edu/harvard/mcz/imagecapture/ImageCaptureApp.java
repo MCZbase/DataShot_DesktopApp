e13431b433943b5c7c3f5e58630b29b12c5b20f4
Paul J. Morris <mole@morris.net>
2017-03-27 10:21:43 -0400

    ISSUE: PURPOSE: Versioning as 1.2.5 for release. DESCRIPTION: In pom and constant.

--- src/main/java/edu/harvard/mcz/imagecapture/ImageCaptureApp.java
+++ src/main/java/edu/harvard/mcz/imagecapture/ImageCaptureApp.java
@@ -63,7 +63,7 @@ public class ImageCaptureApp {
 	
 	private static final Log log = LogFactory.getLog(ImageCaptureApp.class);
 	
-	public static final String APP_VERSION = "1.2.5-SNAPSHOT";
+	public static final String APP_VERSION = "1.2.5";
 	public static final String APP_NAME = "DataShot";
 	public static final String APP_DESCRIPTION = "Rapid capture of data from images of pin Labels and pinned insect \nspecimens developed for the MCZ Lepidoptera collection";
 	public static final String APP_COPYRIGHT = "Copyright © 2009-2017 President and Fellows of Harvard College";
