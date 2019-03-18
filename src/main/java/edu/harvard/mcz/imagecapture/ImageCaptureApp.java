96d40ca51eb6420d6dd07bef74c31965faa65c4c
Paul J. Morris <mole@morris.net>
2019-03-18 11:34:57 -0400

    ISSUE: PURPOSE: Incrementing version to 1.3.1 for release. DESCRIPTION: in pom and application variable.

--- src/main/java/edu/harvard/mcz/imagecapture/ImageCaptureApp.java
+++ src/main/java/edu/harvard/mcz/imagecapture/ImageCaptureApp.java
@@ -73,7 +73,7 @@ public class ImageCaptureApp {
 	
 	private static final Log log = LogFactory.getLog(ImageCaptureApp.class);
 	
-	public static final String APP_VERSION = "1.3.1-SNAPSHOT";
+	public static final String APP_VERSION = "1.3.1";
 	public static final String APP_NAME = "DataShot";
 	public static final String APP_DESCRIPTION = "Rapid capture of data from images of pin Labels and pinned insect \nspecimens developed for the MCZ Lepidoptera collection";
 	public static final String APP_COPYRIGHT = "Copyright © 2009-2017 President and Fellows of Harvard College";
